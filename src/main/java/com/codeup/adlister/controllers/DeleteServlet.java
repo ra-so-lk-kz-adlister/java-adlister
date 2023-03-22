145718d0059423ffe1aafacc0aaeddb551977c7e
Leonard Karoly <leonard.a.karoly@gmail.com>
2023-03-22 15:34:13 -0500

    bootstrap pls work

--- src/main/java/com/codeup/adlister/controllers/DeleteServlet.java
+++ src/main/java/com/codeup/adlister/controllers/DeleteServlet.java
@@ -8,6 +8,7 @@ import javax.servlet.*;
 import javax.servlet.http.*;
 import javax.servlet.annotation.*;
 import java.io.IOException;
+import java.util.List;
 
 @WebServlet(name = "DeleteServlet", value = "/delete")
 public class DeleteServlet extends HttpServlet {
