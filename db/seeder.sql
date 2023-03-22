6d10793955c26473c6ed76e7b08079d65684539b
Songju Oh <songju.oh1@gmail.com>
2023-03-22 09:30:48 -0500

    delete functino

--- db/seeder.sql
+++ db/seeder.sql
@@ -1,10 +1,12 @@
 USE arcade_db;
 
-# INSERT INTO users (user_name, user_email, user_password)
-# VALUES ('leo', 'leo@fake.com', 'password'),
-#        ('sanj', 'sanj@fake.com', 'password'),
-#        ('robert', 'robert@fake.com', 'password'),
-#        ('kailan', 'kailan@fake.com', 'password');
+# DROP TABLE IF EXISTS arcade_ads;
+# #
+# DROP TABLE IF EXISTS users;
+
+INSERT INTO users (username, email, password)
+VALUES ('admin', 'admin@codeup.edu', '$2a$12$Tpjk2mkq4KOLJxgnYXHhveBPiAWC7JVM3SeGtnHvEeFo'),
+       ('osj4129','osj4129@gmail.com','$2a$12$Xrchtvl7H804mAM1yX1zzefldFHsZpoHS1VOk5PBn2ioKyStT8hZa');
 
 INSERT INTO arcade_ads (user_id, ad_name, release_year, rating, description, price)
 VALUES (1 , 'Pacman', 1980, 4, 'Action maze chase, classic coin-op. Must have for any collector!', 1000),
@@ -15,10 +17,10 @@ VALUES (1 , 'Pacman', 1980, 4, 'Action maze chase, classic coin-op. Must have fo
        (1 , 'The Mandoalorian', 2021, 5,
         'In this action-packed pinball quest, players are transported to a galaxy far, far away as they play as the Mandalorian, teaming up with key allies and protecting Grogu, while battling dangerous enemies and forces across their journey.',
         11000),
-       (1 , 'Mortal Kombat', 1993, 4,
+       (2 , 'Mortal Kombat', 1993, 4,
         'Selling my Mortal Kombat 2 Arcade. This would make a nice addition to your game room... This one is in working order.. It has brand new locks with keys / led bulb inside the coin slot.. It also has the two coin buckets as well. I believe this one is in the original cabinet but the previous owner painted the art work in black but I could be wrong... The art work that is currently on it is just a side art sticker I bought.. it doesn''t have the original cover that goes over the control panel and the control panel art isn''t the original either.. Game loads up fine but does have a buzzing sound when the game loads up / game playing.. This is probably a simple fix.. I''ve owned this one for four years.. They do have signs of  scratches / missing wood due to moving from place to place..  Any other questions just send me a message..',
         1500),
-       (1 , 'Turtles In Time', 1991, 4,
+       (2 , 'Turtles In Time', 1991, 4,
         ' this side‑scrolling, ninja weapon‑wielding, beat them up was the fan‑favorite sequel to 1989’s Teenage Mutant Ninja Turtles arcade game.',
         800);
 
