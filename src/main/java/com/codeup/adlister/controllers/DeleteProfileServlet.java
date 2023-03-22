3e6c099d5642eb3a9d2828ed638f944f764b9c3f
Songju Oh <songju.oh1@gmail.com>
2023-03-22 15:12:05 -0500

    delete profile servlet added

--- src/main/java/com/codeup/adlister/controllers/DeleteProfileServlet.java
+++ src/main/java/com/codeup/adlister/controllers/DeleteProfileServlet.java
@@ -1,18 +1,35 @@
+package com.codeup.adlister.controllers;
+
+import com.codeup.adlister.dao.DaoFactory;
+import com.codeup.adlister.models.Ad;
+import com.codeup.adlister.models.User;
 
 import javax.servlet.*;
 import javax.servlet.http.*;
 import javax.servlet.annotation.*;
 import java.io.IOException;
 
-@WebServlet(name = "DeleteProfileServlet", value = "/DeleteProfileServlet")
+@WebServlet(name = "DeleteProfileServlet", value = "/profile/delete")
 public class DeleteProfileServlet extends HttpServlet {
     @Override
     protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
 
+        if (request.getSession().getAttribute("user") == null) {
+            response.sendRedirect("/login");
+            return;
+        }
+        request.getRequestDispatcher("/WEB-INF/deleteprofile.jsp").forward(request, response);
     }
 
     @Override
     protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
 
+        User userCurrent = (User) request.getSession().getAttribute("user");
+        User user = DaoFactory.getUsersDao().findByUsername(userCurrent.getUsername());
+
+        if (user != null && user.getId() == userCurrent.getId() ) {
+            DaoFactory.getUsersDao().deleteUser(user);
+            response.sendRedirect("/logout");
+        }
     }
 }
